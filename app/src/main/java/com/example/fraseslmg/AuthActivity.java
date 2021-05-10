62ca6ee0315724fd0a71c4cb172be3c87bd5aefe
AndoniFiat <andonifiat22@gmail.com>
2021-05-10 03:52:35 +0200

    google login y update user data

--- app/src/main/java/com/example/fraseslmg/AuthActivity.java
+++ app/src/main/java/com/example/fraseslmg/AuthActivity.java
@@ -7,23 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1111;
     private EditText et_email;
     private EditText et_pwd;
     private Button btn_acceder;
     private Button btn_login;
+    private Button btn_google;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
      protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +47,10 @@ public class AuthActivity extends AppCompatActivity {
         et_pwd = (EditText)findViewById(R.id.et_pwd);
         btn_acceder = (Button)findViewById(R.id.btn_acceder);
         btn_login = (Button)findViewById(R.id.btn_login);
+        btn_google = (Button)findViewById(R.id.btn_google);
         mAuth = FirebaseAuth.getInstance();
+
+        initGoogleClient();
 
         btn_acceder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +66,68 @@ public class AuthActivity extends AppCompatActivity {
             }
         });
 
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSigIn();
+            }
+        });
+
+    }
+
+    /**
+     * Iniciamos el cliente de google
+      */
+    private void initGoogleClient() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    /**
+     * Cargamos el intent
+     */
+    private void googleSigIn() {
+        mGoogleSignInClient.signOut();
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // Authenticate with Firebase
+                loginfirebaseWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                showAlert();
+            }
+        }
+    }
+
+    private void loginfirebaseWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {// Sign in success
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            AuthActivity.this.launchHome();
+                        } else { // sign in fails
+                            AuthActivity.this.showAlert();
+                        }
+                    }
+                });
     }
 
     /**
--- app/src/main/java/com/example/fraseslmg/HomeActivity.java
+++ app/src/main/java/com/example/fraseslmg/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.fraseslmg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,16 +8,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView tv_mail;
     private Button btn_logout;
-    FirebaseUser user;
+    private Button btn_save;
+    private Button btn_recover;
+    private EditText et_name;
+    private EditText et_apellidos;
+    private EditText et_municipio;
+    private EditText et_phone;
+    private FirebaseUser user;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
+    public HomeActivity(){}
+
+    public HomeActivity(FirebaseFirestore db) {
+        this.db = db;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +49,23 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btn_logout = (Button)findViewById(R.id.btn_logout);
+        btn_save = (Button)findViewById(R.id.btn_save);
+        btn_recover = (Button)findViewById(R.id.btn_recover);
+        et_name = (EditText)findViewById(R.id.et_name);
+        et_apellidos = (EditText)findViewById(R.id.et_apellidos);
+        et_municipio = (EditText)findViewById(R.id.et_municipio);
+        et_phone = (EditText)findViewById(R.id.et_phone);
         tv_mail = (TextView) findViewById(R.id.tv_mail);
         user = FirebaseAuth.getInstance().getCurrentUser();
         mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+
+
         if (user != null){
             tv_mail.setText(user.getEmail().toString());
         }
@@ -36,6 +75,68 @@ public class HomeActivity extends AppCompatActivity {
                 logout();
             }
         });
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUserData();
+            }
+        });
+
+        btn_recover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserData();
+            }
+        });
+    }
+
+    private void getUserData() {
+        
+    }
+
+    private void setUserData() {
+        if((et_name.getText().length() > 0) && (et_apellidos.getText().length() > 0)
+            && (et_municipio.getText().length() > 0) && (et_phone.getText().length()) > 0){
+            Map<String, Object> object = new HashMap<>();
+            object.put("name", et_name.getText().toString());
+            object.put("last_name", et_apellidos.getText().toString());
+            object.put("city", et_municipio.getText().toString());
+            object.put("phone", et_phone.getText().toString());
+
+            db.collection("users").document(user.getEmail().toString())
+                    .set(object)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(), "Datos añadidos con exito",Toast.LENGTH_LONG).show();
+                            et_name.setText("");
+                            et_apellidos.setText("");
+                            et_municipio.setText("");
+                            et_phone.setText("");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Ha ocurrido un error",Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        }else{
+            if(et_name.getText().length() == 0){
+                et_name.setError("Campo obligatorio");
+            }
+            if(et_apellidos.getText().length() == 0){
+                et_apellidos.setError("Campo obligatorio");
+            }
+            if(et_municipio.getText().length() == 0){
+                et_municipio.setError("Campo obligatorio");
+            }
+            if(et_phone.getText().length() == 0){
+                et_phone.setError("Campo obligatorio");
+            }
+        }
     }
 
     private void logout() {
